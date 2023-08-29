77ee986004ba6cd51942d68adf948ef243c9fe02
KChakhalyan <karenchakhalyan23@gmail.com>
2023-08-29 23:30:32 +0400

    some fixes

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,60 @@
+"use client";
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+
+interface ListingCardProps {
+   data: Listing;
+   reservation?: Reservation;
+   onAction?: (id: string) => void;
+   disabled?: boolean;
+   actionLabel?: string;
+   actionId?: string;
+   currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+   data,
+   reservation,
+   onAction,
+   disabled,
+   actionLabel,
+   actionId = "",
+   currentUser,
+}) => {
+   const router = useRouter();
+   const { getByValue } = useCountries();
+   const location = getByValue(data.locationValue);
+   const handleCancel = useCallback(
+      (e: React.MouseEvent<HTMLButtonElement>) => {
+         e.stopPropagation();
+
+         if (disabled) return;
+
+         onAction?.(actionId);
+      },
+      [onAction, actionId, disabled]
+   );
+
+   const price = useMemo(() => {
+      if (reservation) {
+         return reservation.totalPrice;
+      }
+      return data.price;
+   }, [reservation, data.price]);
+
+   const reservationDate = useMemo(() => {
+      if (!reservation) {
+         return null;
+      }
+
+      const start = new Date(reservation.startDate);
+      const end = new Date(reservation.endDate);
+   }, []);
+
+   return <div>ListingCard</div>;
+};
+
+export default ListingCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-   return <h1 className="text-rose-600 text-4xl">Hello Airbnb</h1>;
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+
+export default async function Home() {
+   const listings = await getListings();
+   const currentUser = await getCurrentUser();
+
+   if (listings.length === 0) {
+      return (
+         <ClientOnly>
+            <EmptyState showReset />
+         </ClientOnly>
+      );
+   }
+
+   return (
+      <ClientOnly>
+         <Container>
+            <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+               {listings.map((listing: any) => {
+                  return <ListingCard key={listing.id} data={listing} currentUser={currentUser} />;
+               })}
+            </div>
+         </Container>
+      </ClientOnly>
+   );
 }
