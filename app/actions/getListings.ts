6ddf4d5c2f367b6accd0fcd32d33708bdcd68da8
KChakhalyan <karenchakhalyan23@gmail.com>
2023-09-04 02:03:27 +0400

    done with Listing Reservation Component

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -5,7 +5,12 @@ export default async function getListings() {
       const listings = await prisma.listing.findMany({
          orderBy: { createdAt: "desc" },
       });
-      return listings;
+      const safeListings = listings.map((listing) => ({
+         ...listing,
+         createdAt: listing.createdAt.toISOString(),
+      }));
+
+      return safeListings;
    } catch (error: any) {
       throw new Error(error);
    }
